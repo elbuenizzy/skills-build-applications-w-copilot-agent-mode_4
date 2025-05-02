51fc0d2490eccde8207b7dbafb4c8ac768364517
Isaac Carrada <isaaccarrada@gmail.com>
2025-05-02 18:55:08 +0000

    Update ALLOWED_HOSTS and base_url for deployment configuration

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -6,7 +6,7 @@ from .models import User, Team, Activity, Leaderboard, Workout
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    base_url = 'https://ominous-lamp-4j76wg7vp9p37g9p.github.dev/'
+    base_url = 'https://ominous-lamp-4j76wg7vp9p-8000.app.github.dev/'
     return Response({
         'users': base_url + 'api/users/',
         'teams': base_url + 'api/teams/',
